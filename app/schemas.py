59360d07a82a7b76e1bc90cfe8d9c872ca2f7b30
sean-maloney <118452234+sean-maloney@users.noreply.github.com>
2025-11-15 22:55:24 +0000

    Added schema, might have to come back to fix

--- app/schemas.py
+++ app/schemas.py
@@ -1,77 +1,98 @@
 from typing import Annotated, Optional, List
 from annotated_types import Ge, Le
-from pydantic import BaseModel, EmailStr, ConfigDict, StringConstraints
+from pydantic import BaseModel, EmailStr, ConfigDict, StringConstraints, Field
  
 # ---------- Reusable type aliases ----------
-NameStr = Annotated[str, StringConstraints(min_length=1, max_length=100)]
-StudentIdStr = Annotated[str, StringConstraints(pattern=r"^S\d{7}$")]
-CodeStr = Annotated[str, StringConstraints(min_length=1, max_length=32)]
-CourseNameStr = Annotated[str, StringConstraints(min_length=1, max_length=255)]
-ProjectNameStr = Annotated[str, StringConstraints(min_length=1, max_length=255)]
-DescStr = Annotated[str, StringConstraints(min_length=0, max_length=2000)]
-AgeInt = Annotated[int, Ge(0), Le(150)]
-CreditsInt = Annotated[int, Ge(1), Le(120)]
+FlightNameStr = Annotated[str, StringConstraints(min_length=1, max_length=100)]
+FlightIdStr = Annotated[str, StringConstraints(pattern=r"^F\d{7}$")]
+FlightOriginStr = Annotated[str, StringConstraints(min_length=1, max_length=32)]
+FlightDestinationStr = Annotated[str, StringConstraints(min_length=1, max_length=255)]
+FlightDepartureTimeStr = Annotated[str, StringConstraints(min_length=5, max_length=5)]
+FlightArrivalTimeStr = Annotated[str, StringConstraints(min_length=5, max_length=5)] #23:10
+FlightDepartureDateStr = Annotated[str, StringConstraints(min_length=10, max_length=10)]
+FlightArrivalDate = Annotated[str, StringConstraints(min_length=10, max_length=10)] #12/11/2025
+FlightPriceStr = Annotated[str, StringConstraints(pattern=r"^€\d{7}$")]
+CompanyNameStr = Annotated[str, StringConstraints(min_length=1, max_length=100)]
+CompanyAirlineCodeStr = Annotated[str, StringConstraints(min_length=1, max_length=3)]
+CompanyCountryStr = Annotated[str, StringConstraints(min_length=2, max_length=100)]
+CompanyEmailStr = EmailStr
+CompanyPhoneStr = Annotated[str, StringConstraints(min_length=5, max_length=20)]
  
-# ---------- Users ----------
-class UserCreate(BaseModel):
-    name: NameStr
-    email: EmailStr
-    age: AgeInt
-    student_id: StudentIdStr
+# ---------- Flight ----------
+class FlightCreate(BaseModel):
+    name: FlightNameStr
+    flight_id: FlightIdStr
+    origin: FlightOriginStr
+    destination: FlightDestinationStr
+    departure_time: FlightDepartureTimeStr
+    arrival_time: FlightArrivalTimeStr
+    depature_date: FlightDepartureDateStr
+    arrival_date: FlightArrivalDate
+    price: FlightPriceStr
  
-#Added as needed to make patch endpoint work
-class UserUpdate(BaseModel):
-    name: Optional[NameStr] = None
-    email: Optional[EmailStr] = None
-    age: Optional[AgeInt] = None
-    student_id: Optional[StudentIdStr] = None
+class FlightUpdate(BaseModel):
+    name: FlightNameStr
+    flight_id: FlightIdStr
+    origin: FlightOriginStr
+    destination: FlightDestinationStr
+    departure_time: FlightDepartureTimeStr
+    arrival_time: FlightArrivalTimeStr
+    depature_date: FlightDepartureDateStr
+    arrival_date: FlightArrivalDate
+    price: FlightPriceStr
  
-class UserRead(BaseModel):
+class FlightRead(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    name: NameStr
-    email: EmailStr
-    age: AgeInt
-    student_id: StudentIdStr
- 
-# Optionally return users with their projects
-class ProjectRead(BaseModel):
+    name: FlightNameStr
+    flight_id: FlightIdStr
+    origin: FlightOriginStr
+    destination: FlightDestinationStr
+    departure_time: FlightDepartureTimeStr
+    arrival_time: FlightArrivalTimeStr
+    depature_date: FlightDepartureDateStr
+    arrival_date: FlightArrivalDate
+    price: FlightPriceStr
+
+
+# ---------- Company ----------
+class CompanyRead(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    project_id: int
-    name: ProjectNameStr
-    description: Optional[DescStr] = None
-    owner_id: int
+    company_id: int
+    code: CompanyAirlineCodeStr
+    name: CompanyNameStr
+    country: CompanyCountryStr
+    email: CompanyEmailStr
+    phone: CompanyPhoneStr
  
-class UserReadWithProjects(UserRead):
-    projects: List[ProjectRead] = []
+class FlightReadWithCompany(FlightRead):
+    company: Optional[CompanyRead] = None
  
-# ---------- Projects ----------
-# Flat route: POST /api/projects (owner_id in body)
-class ProjectCreate(BaseModel):
-    name: ProjectNameStr
-    description: Optional[DescStr] = None
-    owner_id: int
+class CompanyCreate(BaseModel):
+    code: CompanyAirlineCodeStr         
+    name: CompanyNameStr        
+    country: CompanyCountryStr
+    email: CompanyEmailStr
+    phone: CompanyPhoneStr
+
+class FlightCreateForCompany(BaseModel):
+    name: FlightNameStr
+    flight_id: FlightIdStr
+    origin: FlightOriginStr
+    destination: FlightDestinationStr
+    departure_time: FlightDepartureTimeStr
+    arrival_time: FlightArrivalTimeStr
+    departure_date: FlightDepartureDateStr
+    arrival_date: FlightArrivalDate
+    price: FlightPriceStr
  
-# Nested route: POST /api/users/{user_id}/projects (owner implied by path)
-class ProjectCreateForUser(BaseModel):
-    name: ProjectNameStr
-    description: Optional[DescStr] = None
+class CompanyUpdate(BaseModel):
+    code: Optional[CompanyAirlineCodeStr] = None
+    name: Optional[CompanyNameStr] = None
+    country: Optional[CompanyCountryStr] = None
+    email: Optional[CompanyEmailStr] = None
+    phone: Optional[CompanyPhoneStr] = None
  
-#Added as needed to make patch endpoint work
-class ProjectUpdate(BaseModel):
-    name: Optional[ProjectNameStr] = None
-    description: Optional[DescStr] = None
-    owner_id: Optional[int] = None
+class FlightReadCompany(CompanyRead):
+    flights: Optional[FlightRead] = Field(default_factory=list) 
  
-class ProjectReadWithOwner(ProjectRead):
-    owner: Optional["UserRead"] = None # use selectinload(ProjectDB.owner) when querying
- 
-# ---------- Courses ----------
-class CourseCreate(BaseModel):
-    code: CodeStr
-    name: CourseNameStr
-    credits: CreditsInt
- 
-class CourseRead(CourseCreate):
-    model_config = ConfigDict(from_attributes=True)
-    id: int
